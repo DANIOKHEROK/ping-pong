2c9677f1dec02694fb8716638628724dcaa810e6
onl1xxx <103316429+onl1xxx@users.noreply.github.com>
2022-04-09 16:07:10 +0300

    Update game.py

--- game.py
+++ game.py
@@ -17,7 +17,25 @@ class Player(GameSprite):
         
 
 #3
+back = (250,255,255)
+wind_width = 600
+win_height = 500
+window = display.set_mode((wind_width, win_height))
+window.fill(back)
 
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font('calibri.ttf', 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 #4
 
 #5
